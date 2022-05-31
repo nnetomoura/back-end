b574e4449692af58020e44a7763967697bc75c3f
Claudio Pereira <claudio.prepara0@gmail.com>
2022-05-31 10:08:47 -0300

    back-end do blog pessoal finalizado. Swagger config adicionado e funcionando

--- blogpessoal/src/main/java/com/generation/blogpessoal/model/Usuario.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -16,6 +16,8 @@ import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 @Entity
 @Table (name = "tb_usuarios")
 public class Usuario {
@@ -27,6 +29,7 @@ public class Usuario {
 	@NotNull (message = "O atributo *nome* é obrigatório!")
 	private String nome;
 	
+	@Schema (example = "email@mail.com.br")
 	@NotNull (message = "O atributo *usuário* é obrigatório!")
 	@Email (message = "O atributo *usuário* deve ser um e-mail válido!")
 	private String usuario;
